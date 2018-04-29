04adc5aa302252012330a5d6324b0a202cf9dadc
Tomás Migone <tomasmigone@gmail.com>
2018-04-29 03:26:44 -0300

    added oauth endpoints. getting token now from bgh service

--- server.js
+++ server.js
@@ -1,52 +1,58 @@
 const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
 const session = require('cookie-session')
+const bodyParser = require('body-parser')
+const bgh = require('./lib/bgh')
 
 const app = express()
+app.set('view engine', 'pug')
 app.set('trust proxy', 1)
 app.set('port', 5000)
-app.use(session({ secret: 'batata', cookie: { maxAge: 2592000 }}))
-// c9rvt7y / alexa1234
+app.use(session({ secret: 'batata', cookie: { maxAge: 2592000 } }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-
+// Test endpoint
 app.get('/', (req, res) => {
-  res.send("Hello!")
+  res.json({ message: 'ok' })
 })
 
-
+// OAuth 2.0 - Request token endpoints
 app.get('/oauth/request_token', (req, res) => {
-  let sess = req.session
-  sess.state = req.query.state
-  sess.client_id = req.query.client_id
-  sess.scope = req.query.scope
-  sess.redirect_uri = req.query.redirect_uri
-  console.log(sess)
-
-  let redirect_alexa = decodeURI(sess.redirect_uri)+"?state="+sess.state+"&code="+"SplxlOBeZQQYbYS6WxSbIA"
-  console.log(redirect_alexa)
-
-  res.redirect(redirect_alexa)
+  let data = {
+    state: req.query.state,
+    redirectURL: req.query.redirect_uri
+  }
+  if (data.state && data.redirectURL) {
+    res.render('login', data)
+  } else {
+    res.status(500).send('Error: Did not receive state or redirect_uri.')
+  }
 })
 
-app.get('/oauth/access_token', (req, res) => {
-	let sess = req.session
-	res.json({
-    access_token: "SplxlOBeZQQYbYS6WxSbIR",
-    refresh_token: "SplxlOBeZQQYbYS6WxSbIR"
+app.get('/oauth/request_token/callback', (req, res) => {
+  let device = new bgh.Device()
+  device.login(req.query.uname, req.query.psw).then(() => {
+    if (req.query.redirectURL && req.query.state && device.token) {
+      let redirectURL = decodeURI(req.query.redirectURL) + '?state=' + req.query.state + '&code=' + device.token
+      res.redirect(redirectURL)
+    } else {
+      res.status(500).send('Error: Could not get redirectURL, state or token.')
+    }
+  }).catch((error) => {
+    res.status(500).send(error)
   })
 })
 
+// OAuth 2.0 - Access token endpoint
 app.post('/oauth/access_token', (req, res) => {
-	let sess = req.session
-
-	res.json({
-    access_token: "SplxlOBeZQQYbYS6WxSbIR",
-    refresh_token: "SplxlOBeZQQYbYS6WxSbIR"
-  })
+  if (req.body.code) {
+    res.json({
+      access_token: 'SplxlOBeZQQYbYS6WxSbIR'
+    })
+  }
 })
 
-
+// Start server
 app.listen(app.get('port'), () => {
-  console.log('Express server started on port 5000')
+  console.log('Listening on port %s', app.get('port'))
 })
