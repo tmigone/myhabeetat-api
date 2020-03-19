e80c7849a647455ad91bf81930e3d2d7eded6b55
Tomás Migone <tomasmigone@gmail.com>
2020-03-18 23:08:55 -0300

    Update dependencies
    Change-type: major
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const session = require('cookie-session')
-const bodyParser = require('body-parser')
-const BGH = require('bgh-smart-control')
+const MyHabeetat = require('myhabeetat')
 
 const app = express()
 app.set('view engine', 'pug')
 app.set('trust proxy', 1)
 app.set('port', 5000)
 app.use(session({ secret: 'batata', cookie: { maxAge: 2592000 } }))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Test endpoint
 app.get('/', (req, res) => {
   res.json({ message: 'ok' })
 })
 
-// OAuth 2.0 - Request token endpoints
+// OAuth 2.0 - Request token endpoints - Authorization URI
 app.get('/oauth/request_token', (req, res) => {
   let data = {
     state: req.query.state,
@@ -30,16 +29,19 @@ app.get('/oauth/request_token', (req, res) => {
 })
 
 app.get('/oauth/request_token/callback', async (req, res) => {
-  let bgh = new BGH()
-  let token = await bgh.login(req.query.uname, req.query.psw)
-  if (req.query.redirectURL && req.query.state && token) {
-    res.redirect(decodeURI(req.query.redirectURL) + '?state=' + req.query.state + '&code=' + token)
-  } else {
-    res.status(500).send('Error: Could not get redirectURL, state or token.')
+  try {
+    let token = await MyHabeetat.login(req.query.uname, req.query.psw)
+    if (req.query.redirectURL && req.query.state && token) {
+      res.redirect(decodeURI(req.query.redirectURL) + '?state=' + req.query.state + '&code=' + token)
+    } else {
+      res.status(500).send('Error: Could not get redirectURL, state or token.')
+    }
+  } catch (error) {
+    res.status(500).send(`Error: ${error}`)
   }
 })
 
-// OAuth 2.0 - Access token endpoint
+// OAuth 2.0 - Access token endpoint - Access token URI
 app.post('/oauth/access_token', (req, res) => {
   if (req.body.code) {
     res.json({
