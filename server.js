7bc0af0479c71714efde1f579a39231e2b213553
Tomás Migone <tomasmigone@gmail.com>
2020-04-11 18:09:44 -0300

    Multiple changes
    + Rename project
    + Restyle OAuth login page
    + Fix pug engine root directory
    + Added healthchek endpoint
    
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- server.js
+++ server.js
@@ -2,22 +2,20 @@ const packageJSON = require('./package.json')
 const express = require('express')
 
 const { errorHandler } = require('./middleware/error.js')
+const healthRoutes = require('./routes/health.js')
 const oauthRoutes = require('./routes/oauth.js')
 const apiRoutes = require('./routes/api/index.js')
 // const explorerRoutes = require('./routes/explorer.js')
 
 const app = express()
+app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 app.set('trust proxy', 1)
 app.set('port', 80)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Test endpoint
-app.get('/', (req, res) => {
-  res.json({ status: 'ok' })
-})
-
+app.use('/health', healthRoutes)
 // app.use('/', explorerRoutes)
 app.use('/v1/oauth/', oauthRoutes)
 app.use('/v1/api/', apiRoutes)
